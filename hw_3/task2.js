09c86481ad0787786df548463c9b00a4b75cd0fd
OlexandrManziuk <radnik.av@ukr.net>
2020-03-22 16:36:46 +0200

    fixed_hw_03

---
+++ hw_3/task2.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const countProps = function(obj) {
+  // твой код
+  return Object.entries(obj).length;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(
+  countProps({
+    name: "Mango",
+    age: 2
+  })
+); // 2
+
+console.log(
+  countProps({
+    mail: "poly@mail.com",
+    isOnline: true,
+    score: 500
+  })
+); // 3
