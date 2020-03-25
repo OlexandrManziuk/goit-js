141f61a527e4bdcce7a8a9dfc0f4a67e4b90850c
OlexandrManziuk <radnik.av@ukr.net>
2020-03-25 19:14:28 +0200

    fixed_methods_calls

--- hw_4/task2.js
+++ hw_4/task2.js
@@ -4,28 +4,26 @@ const inventory = {
   items: ["Knife", "Gas mask"],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
-
-    inventory.items.push(itemName);
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
-
-    inventory.items = inventory.items.filter(item => item !== itemName);
+    this.items = this.items.filter(item => item !== itemName);
   }
 };
 
 const invokeInventoryAction = function(itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+  action.call(this, itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction.bind(inventory, "Medkit", inventory.add)();
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction.call(inventory, "Gas mask", inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
